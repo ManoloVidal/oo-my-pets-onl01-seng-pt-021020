b79e6457b190758cd4a8ef2acc9e2497fd52937e
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-11 18:14:04 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,6 +33,10 @@ class Owner
     Cat.all.select {|cat| cat.owner == self}
   end 
   
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self} 
+  end 
+  
    def buy_cat(name)
     cat = Cat.new(name)
     self.pets[:cats] << cat
