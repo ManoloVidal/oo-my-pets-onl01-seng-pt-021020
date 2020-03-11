9560422d98ebbfb6b1e3ccee75c6c5ac12a82b58
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-11 18:10:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
 class Owner
-  # code goes here
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+  @@count = 0 
+  
+  attr_reader :name, :species
+  attr_accessor :pets, :cats, :dogs 
+  
+  def initialize(name, species = "human")
+    @name = name 
+    @species = species
+    @@all << self 
+    @@count +=1 
+  end 
+
+  def say_species
+    "I am a #{@species}."
+  end 
+ 
+  def self.all
+    @@all 
+  end 
+  
+  def self.count 
+    @@all.size 
+  end 
+  
+  def self.reset_all  
+    @@all.clear
+  end 
+  
+   def buy_cat(name)
+    cat = Cat.new(name)
+    self.pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    self.pets[:dogs] << dog
+  end
+    
+   
 end
